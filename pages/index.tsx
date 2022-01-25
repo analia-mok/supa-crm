41ce5133483f32432b44b8f39572c6705442fd22
Analia Mok <analiam319@gmail.com>
2022-01-24 21:38:43 -0500

    implemented initial supabase client and first query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import { supabase } from '../utils/supabase';
+
+interface Organization {
+  id: string,
+  name: string,
+  email?: string,
+  phone?: string,
+  address1?: string,
+  address2?: string,
+  city?: string,
+  state?: string,
+  postal_code?: string,
+}
+
+export default function Home(props: { organizations: Organization[] }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      Hi! World!
+      {props.organizations.map(org => (
+        <p key={org.id}>{org.name}</p>
+      ))}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const { data: organizations } = await supabase.from('organization').select('*');
+
+  return {
+    props: {
+      organizations
+    }
+  };
 }
